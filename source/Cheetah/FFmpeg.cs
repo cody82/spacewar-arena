5d3e7b71a902f6619a51821710f079a759f618f5
cody <devnull@localhost>
2010-10-03 20:25:21 +0200

    FFmpeg test code, doesnt work (avformat-52.dll not found??)

---
+++ source/Cheetah/FFmpeg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Tao.FFmpeg;
+
+namespace Cheetah
+{
+    public class FFmpegFile
+    {
+        static FFmpegFile()
+        {
+            FFmpeg.av_register_all();
+        }
+
+        IntPtr context;
+
+        IntPtr OpenFile(string filename)
+        {
+            IntPtr context;
+            if (FFmpeg.av_open_input_file(out context, filename, IntPtr.Zero, 0, IntPtr.Zero) != 0)
+            {
+                throw new Exception("FFmpeg: cant open file " + filename);
+            }
+            return context;
+        }
+
+        public FFmpegFile(string filename)
+        {
+            context = OpenFile(filename);
+        }
+
+        public FFmpegFile()
+        {
+        }
+    }
+}
