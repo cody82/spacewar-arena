591fc9d2c2eff1bd066558ed2a8e8a4d47000354
cody <devnull@localhost>
2010-10-15 22:15:54 +0200

    ASP.NET game server list.

---
+++ source/AspServerList/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Startseite" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="AspServerList._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Willkommen bei ASP.NET!
+    </h2>
+    <p>
+        Weitere Informationen zu ASP.NET finden Sie auf <a href="http://www.asp.net" title="ASP.NET-Website">www.asp.net</a>.
+    </p>
+    <p>
+        <a href="http://go.microsoft.com/fwlink/?LinkID=152368"
+            title="MSDN-ASP.NET-Dokumente">Dokumentation finden Sie auch unter ASP.NET bei MSDN</a>.
+    </p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server" Width="100%">
+        </asp:GridView>
+    </p>
+</asp:Content>
---
+++ source/AspServerList/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Cheetah;
+using Lidgren.Network;
+using System.Threading;
+
+namespace AspServerList
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        List<SpaceWar2006.GameSystem.GameServerInfo> infos = new List<SpaceWar2006.GameSystem.GameServerInfo>();
+
+        protected void Answer(NetIncomingMessage msg)
+        {
+            SpaceWar2006.GameSystem.GameServerInfo info;
+            {
+                string p = msg.ReadString();
+                info = new SpaceWar2006.GameSystem.GameServerInfo(p);
+                infos.Add(info);
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ServerFinder finder = new ServerFinder(new ServerFinder.AnswerDelegate(Answer), false, true);
+
+            GridView1.AutoGenerateColumns = true;
+
+            for (int i = 0; i < 5; ++i)
+            {
+                finder.Tick(0.5f);
+                Thread.Sleep(500);
+            }
+
+            GridView1.DataSource = infos;
+            GridView1.DataBind();
+        }
+    }
+}
